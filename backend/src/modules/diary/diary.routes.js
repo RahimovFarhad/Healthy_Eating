f7669828a8ae81e96610b1f92fe4426d798d1609
Farhad Rahimov <farhad.r2006@mail.ru>
2026-03-16 19:25:59 +0000

    incomplete note changed for mimi

--- backend/src/modules/diary/diary.routes.js
+++ backend/src/modules/diary/diary.routes.js
@@ -8,7 +8,7 @@ diaryRouter.get("/summary", getSummary);
 diaryRouter.get("/entries", listDiaryEntries); 
 diaryRouter.get("/entries/:id", getDiaryEntryById); // incomplete, should check if current user is trying to access their own diary entry
 diaryRouter.delete("/entries/:id", deleteEntry); //incomplete, should check if current user is trying to delete their own diary entry
-diaryRouter.post("/entries/:id/items", createDiaryEntryItem); // incomplete: user can only add to their own diary; If entry item is custom, first create it. 
+diaryRouter.post("/entries/:id/items", createDiaryEntryItem); // incomplete: user can only add to their own diary  
 diaryRouter.patch("/entry-items/:itemId", updateDiaryEntryItem); // incomplete, check if user is trying to update their own diary entry item;
 diaryRouter.delete("/entry-items/:itemId", deleteEntryItem); // incomplete, check if user is trying to delete their own diary entry item;
 
